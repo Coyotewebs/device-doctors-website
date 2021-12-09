363f24ecf0913f70a31d921f311dc93a91d628d1
Coyotewebs <victor@coyotewebs.com>
2021-12-09 14:25:21 -0700

    fix slider
    more..

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 
+const slider = document.querySelector(".about-us__slider")
 const nextBtn = document.querySelector(".next-btn")
 const prevBtn = document.querySelector(".prev-btn")
 const slides  = document.querySelectorAll(".about-us__slide")
@@ -27,15 +28,64 @@ nextBtn.addEventListener("click", () => {
   slideIcons[slideNumber].classList.add("active")
 })
 
+//image slider previous button
 
 
+prevBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove('active')
+  })
 
+  slideNumber--;
 
+  if(slideNumber < 0) {
+    slideNumber = numberOfSlides - 1;
+  }
+  
+  slides[slideNumber].classList.add("active")
+  slideIcons[slideNumber].classList.add("active")
+})
 
+//image slider autoplay
 
+var playSlider;
 
+var repeater = () => {
+  playSlider = setInterval(function(){
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+    slideIcons.forEach((slideIcon) => {
+      slideIcon.classList.remove('active')
+    })
+  
+    slideNumber++
+  
+    if(slideNumber > (numberOfSlides - 1)) {
+      slideNumber = 0
+    }
+    
+    slides[slideNumber].classList.add("active")
+    slideIcons[slideNumber].classList.add("active")
 
+  }, 5000);
+}
+repeater();
 
+//stop the image slider autoplay on mouseover 
+
+slider.addEventListener("mouseover", () => {
+  clearInterval(playSlider);
+})
+
+//start the image slider autoplay again on mouseout
+
+slider.addEventListener("mouseout", () => {
+  repeater();
+})
 
 
 
