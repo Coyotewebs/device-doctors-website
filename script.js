a838bf7a02697cc9f16a659c11f8c80aa1db3033
Coyotewebs <88667536+Coyotewebs@users.noreply.github.com>
2022-02-17 11:14:39 -0600

    Changes for Steve
    redesign of the nav bar, mobile nav  and some work on acc

--- script.js
+++ script.js
@@ -8,11 +8,75 @@ const slideIcons = document.querySelectorAll(".slide-icon")
 const numberOfSlides = slides.length
 var slideNumber = 0 
 
+
+// Start of dynamic copyright 
 const yearSpan = document.querySelector('#currentYear')
 const currentYear = new Date();
 yearSpan.innerText = currentYear.getFullYear()
+// End of dynamic copyright 
 
 
+// Toggle menu icons
+// const menu = document.querySelector(".menu")
+// const navContents = document.querySelector("#nav_contents")
+
+// menu.addEventListener("click", () => {
+//   navContents.classList.toggle("toggle")
+// })
+
+//new
+
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+
+      //show
+toggleMenu.addEventListener('click', ()=> {
+  navMenu.classList.toggle('show')
+})
+
+//hidden
+
+closeMenu.addEventListener('click', ()=>{
+  navMenu.classList.remove('show')
+})
+
+//active 
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+  navLink.forEach(n=> n.classList.remove('active'))
+  this.classList.add('active')
+
+  // Remove menu mobile
+  navMenu.classList.remove('show')
+
+}
+
+
+
+navLink.forEach(n => n.addEventListener('click',linkAction ))
+
+
+
+
+
+
+
+
+
+
+
+
+//old
+//Start of toggleBox
+// function showMenu(){
+//   document.querySelector('.navigation').classList.toggle('active')
+//   document.querySelector('.menu').classList.toggle('hide')
+//   document.querySelector('.close').classList.toggle('show')
+// }
+
+//End toggleBox
 
 
 //image slider next button
